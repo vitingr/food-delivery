5658abf8e92761688a1712d8d1786988a5e1b292
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-10-19 17:56:51 -0300

    Update Category Feature

--- app/restaurant/[id]/page.tsx
+++ app/restaurant/[id]/page.tsx
@@ -1,8 +1,192 @@
-import React from 'react'
+"use client"
+
+import { useSession } from 'next-auth/react'
+import React, { useEffect, useState, useRef } from 'react'
+import { RiVerifiedBadgeFill } from 'react-icons/ri'
+import { AiFillStar } from 'react-icons/ai'
+import { BsCoin } from 'react-icons/bs'
+import { useRouter } from 'next/navigation'
+import { APP_ROUTES } from '@/constants/app-routes'
+import { toast } from 'react-toastify'
+import ToastMessage from '@/components/Config/ToastMessage'
+import Link from 'next/link'
+import { RestaurantData } from '@/types/types'
 
 const page = () => {
-  return (
-    <div>page</div>
+
+  // Usar sistema do pathname para substituir o session?.user?.email
+  // Alterar nestjs e adicionar uma rota de GET para pegar valores pelo ID
+  // Alterar valores restaurantID
+
+  const { data: session, status } = useSession()
+
+  const isFetched = useRef(false)
+
+  const router = useRouter()
+
+  const [restaurantData, setRestaurantData] = useState<RestaurantData | any>([])
+  const [categories, setCategories] = useState<any>([])
+  const [products, setProducts] = useState<any>([])
+
+  const [isOwner, setIsOwner] = useState<boolean>(false)
+
+  const getRestaurantData = async () => {
+    const requisition = await fetch(`http://localhost:3001/restaurant/${session?.user?.email}`)
+    const response = await requisition.json()
+
+    if (response !== null) {
+      if (response.email === session?.user?.email) {
+        setRestaurantData(response)
+        setIsOwner(true)
+
+        getRestaurantCategories(response.id)
+      } else {
+        toast.error("Você não pode editar um restaurante que não é seu")
+        router.push(APP_ROUTES.private.usuario)
+      }
+    } else {
+      toast.error("Você não pode editar um restaurante que não existe")
+      router.push(APP_ROUTES.private.usuario)
+    }
+  }
+
+  const getRestaurantCategories = async (restaurantId: string) => {
+    try {
+      const requisition = await fetch(`http://localhost:3001/category/${restaurantId}`)
+      const response = await requisition.json()
+
+      setCategories(response)
+
+      getRestaurantProducts(restaurantId)
+
+    } catch (error) {
+      console.log(error)
+      throw new Error("Não foi possível obter as categorias de produtos do restaurante")
+    }
+  }
+
+  const getRestaurantProducts = async (restaurantId: string) => {
+    try {
+      const requisition = await fetch(`http://localhost:3001/product/${restaurantId}`)
+      const response = await requisition.json()
+
+      setProducts(response)
+
+    } catch (error) {
+      console.log(error)
+      throw new Error("Não foi possível obter os produtos do restaurante")
+    }
+  }
+
+  useEffect(() => {
+    if (!isFetched.current) {
+      if (session?.user?.email !== undefined && status === "authenticated") {
+        getRestaurantData()
+      }
+    } else {
+      isFetched.current = true
+    }
+  }, [])
+
+  return isOwner === true ? (
+    <div className='bg-[#f2f2f2] w-full min-h-[62vh] flex flex-col items-center p-[2%]'>
+      <ToastMessage />
+      <div className='bg-white max-w-[1300px] w-full rounded-sm p-16'>
+        <div className='bg-[url("https://www.ifood.com.br/static/images/merchant/banner/DEFAULT.png")] bg-cover bg-no-repeat w-full h-[200px] rounded-xl' />
+        <div className='mt-10 flex justify-between w-full'>
+          <div className='flex gap-6 w-full'>
+            <img src={restaurantData.logo} className='rounded-full w-[80px] h-[80px]' alt="Restaurant Image" />
+            <div>
+              <div className='flex items-center gap-2'>
+                <h1 className='text-2xl'>{restaurantData.restaurantName}</h1>
+                <RiVerifiedBadgeFill size={22.5} className="red-icon" />
+              </div>
+              <div>
+                <h5 className='text-[#717171] text-sm'>Especialidade da casa: {restaurantData.speciality}</h5>
+                <h6 className='text-[#717171] text-sm'>Tempo de entrega: ≅ {restaurantData.deliveryTime}</h6>
+              </div>
+            </div>
+          </div>
+          <div className=''>
+            <div className='flex gap-8 h-[25px]'>
+              <div className='flex gap-3 items-center'>
+                <AiFillStar size={15} className="gold-icon" />
+                <h3>{restaurantData.stars}.0</h3>
+              </div>
+              <div className='w-[200px] flex items-center gap-3 pl-8 border-l border-neutral-300'>
+                <BsCoin size={15} className="gray-icon" />
+                <p className='text-sm text-[#717171]'>Pedido mínimo R$ {restaurantData.deliveryValue}5,00</p>
+              </div>
+            </div>
+            <div className='mt-2 flex gap-4'>
+              <div className='w-full'>
+                <h3 className='text-sm text-[#717171]'>Brasil - {restaurantData.state}, {restaurantData.city}</h3>
+                <h5 className='text-base text-[#717171]'>{restaurantData.street}, {restaurantData.address}</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='bg-white w-full min-h-[18vh] p-16 mt-[75px] max-w-[1300px]'>
+        <h1 className='w-full text-center text-4xl font-bold selection:bg-[#ea1d2c] selection:text-white mb-[50px]'>Menu do Restaurante</h1>
+        {categories.length > 0 ? (
+          <>
+            {categories.map((category: {
+              id: string,
+              restaurant: string,
+              categoryName: string,
+              categoryDescription: string,
+              quantityItems: number
+            }) => (
+              <div className='mt-[75px]' key={category.id}>
+                <div>
+                  <div className='flex items-end gap-2'>
+                    <h2 className='font-bold text-2xl'>{category.categoryName}</h2>
+                    {category.quantityItems === 0 ? <h5 className='text-sm'>Categoria vazia</h5> : <h5 className='text-sm'>{category.quantityItems} itens na categoria</h5>}
+                  </div>
+                  <h6 className='text-base text-[#717171] '>{category.categoryDescription}</h6>
+                </div>
+                <div className='mt-16 grid grid-cols-2 gap-8'>
+                  {products.map((product: {
+                    id: string,
+                    restaurant: string,
+                    category: string,
+                    productName: string,
+                    productDescription: string,
+                    productValue: number,
+                    productFoto: string,
+                  }) => (
+                    <>
+                      {product.category === category.id ? (
+                        <>
+                          <div className='flex justify-between p-6 border border-neutral-100 rounded-lg h-[250px] w-full shadow-sm cursor-pointer transition-all duration-300 hover:border-neutral-300'>
+                            <div className='flex flex-col justify-center w-full'>
+                              <div className='h-full '>
+                                <h1 className='text-2xl font-bold'>{product.productName}</h1>
+                                <h2 className='text-[#717171] text-sm mt-4'>{product.productDescription}</h2>
+                              </div>
+                              <h5 className='text-xl'>{product.productValue.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h5>
+                            </div>
+                            <div>
+                              <img src={product.productFoto} className='w-[175px] h-[175px]' alt="Product Image" />
+                            </div>
+                          </div>
+                        </>
+                      ) : (<></>)}
+                    </>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <p>Esse restaurante ainda não adicionou nada ao seu cardápio</p>
+        )}
+      </div>
+    </div>
+  ) : (
+    <></>
   )
 }
 
---
+++ components/Restaurant/EditItems/Menu.tsx
@@ -0,0 +1,344 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import { IoAddOutline } from 'react-icons/io5'
+import Upload from '@/components/Config/Upload'
+import Popup from '@/components/Popup/Popup'
+import Button from '@/components/Button'
+
+const Menu = ({ restaurantId }: { restaurantId: string }) => {
+
+  const [createProductCategory, setCreateProductCategory] = useState<boolean>(false)
+  const [createProduct, setCreateProduct] = useState<boolean>(false)
+  const [editProductCategory, setEditProductCategory] = useState<boolean>(false)
+  const [currentProductCategory, setCurrentProductCategory] = useState<string>("")
+
+  const [currentCategoryId, setCurrentCategoryId] = useState<string>("")
+  const [categoryName, setCategoryName] = useState<string>("")
+  const [categoryDescription, setCategoryDescription] = useState<string>("")
+  const [productName, setProductName] = useState<string>("")
+  const [productDescription, setProductDescription] = useState<string>("")
+  const [productValue, setProductValue] = useState<string>("")
+  const [productFoto, setProductFoto] = useState<any>("")
+
+  const [categories, setCategories] = useState<any>([])
+  const [products, setProducts] = useState<any>([])
+
+  const createCategory = async () => {
+    if (categoryName !== "" && categoryDescription !== "") {
+      try {
+        const response = await fetch("http://localhost:3001/category/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            restaurant: restaurantId,
+            categoryName: categoryName,
+            categoryDescription: categoryDescription
+          })
+        })
+
+        if (response.ok) {
+          toast.success("Categoria criada com Sucesso!")
+          getRestaurantCategories(restaurantId)
+          setCreateProductCategory(false)
+        } else {
+          toast.error("Não foi possível adicionar a categoria")
+        }
+
+      } catch (error) {
+        console.log(error)
+        toast.error("Não foi possível adicionar a categoria")
+      }
+    }
+  }
+
+  const editCategory = async (categoryId: string) => {
+    setEditProductCategory(true)
+    setCurrentProductCategory(categoryId)
+  }
+
+  const createNewProduct = async () => {
+    if (productName !== "" && productValue !== "" && productDescription !== "" && productFoto !== "") {
+      try {
+        const response = await fetch("http://localhost:3001/product/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            restaurant: restaurantId,
+            category: currentCategoryId,
+            productName: productName,
+            productDescription: productDescription,
+            productValue: Number(productValue),
+            productFoto: productFoto
+          })
+        })
+
+        if (response.ok) {
+          toast.success("Produto criado com Sucesso!")
+          setProductFoto("")
+          getRestaurantProducts(restaurantId)
+          setCreateProduct(false)
+        } else {
+          toast.error("Não foi possível adicionar o produto")
+        }
+
+      } catch (error) {
+        console.log(error)
+        toast.error("Não foi possível adicionar o produto")
+      }
+    }
+  }
+
+  const getRestaurantCategories = async (restaurantId: string) => {
+    try {
+
+      const requisition = await fetch(`http://localhost:3001/category/${restaurantId}`)
+      const response = await requisition.json()
+
+      setCategories(response)
+
+      getRestaurantProducts(restaurantId)
+
+    } catch (error) {
+      console.log(error)
+      throw new Error("Não foi possível obter as categorias de produtos do restaurantes")
+    }
+  }
+
+  const getRestaurantProducts = async (restaurantId: string) => {
+    try {
+      const requisition = await fetch(`http://localhost:3001/product/${restaurantId}`)
+      const response = await requisition.json()
+
+      setProducts(response)
+
+    } catch (error) {
+      console.log(error)
+      throw new Error("Não foi possível obter os produtos do restaurante")
+    }
+  }
+
+  const createProductAction = async (restaurantId: string) => {
+    setCreateProduct(true)
+    setCurrentCategoryId(restaurantId)
+  }
+
+  const removeCategory = async (categoryId: string) => {
+    if (categoryId) {
+      try {
+        const response = await fetch("http://localhost:3001/category/remove", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: categoryId
+          })
+        })
+
+        if (response.ok) {
+          toast.success("Categoria removida do seu cardápio")
+          getRestaurantCategories(restaurantId)
+        } else {
+          toast.error("Não foi possível remover a categoria")
+        }
+
+      } catch (error) {
+        console.log(error)
+        toast.error("Não foi possível remover a categoria")
+      }
+    }
+  }
+
+  const updateCategory = async (categoryId: string) => {
+    if (categoryId) {
+      try {
+        const response = await fetch("http://localhost:3001/category/update", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            id: categoryId,
+            restaurant: restaurantId,
+            categoryName: categoryName,
+            categoryDescription: categoryDescription,
+            quantityItems: Number(0)
+          })
+        })
+
+        if (response.ok) {
+          toast.success("Categoria editada com sucesso!")
+          setEditProductCategory(false)
+          getRestaurantCategories(restaurantId)
+        } else {
+          toast.error("ERRO! Não foi possível editar a categoria")
+        }
+
+      } catch (error) {
+        console.log(error)
+        toast.error("ERRO! Não foi possível editar a categoria")
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (restaurantId !== undefined) {
+      getRestaurantCategories(restaurantId)
+    }
+  }, [])
+
+  return (
+    <>
+      {categories.length > 0 ? (
+        <>
+          {categories.map((category: {
+            id: string,
+            restaurant: string,
+            categoryName: string,
+            categoryDescription: string,
+            quantityItems: number
+          }) => (
+            <div className='mt-[50px] p-6 border border-neutral-200 rounded-xl' key={category.id}>
+              <div>
+                <div className='flex items-end gap-2'>
+                  <h2 className='font-bold text-2xl'>{category.categoryName}</h2>
+                  {category.quantityItems === 0 ? <h5 className='text-sm mb-[1.5px]'>Categoria vazia</h5> : <h5 className='text-sm -m-1'>{category.quantityItems} itens na categoria</h5>}
+                </div>
+                <h6 className='text-base text-[#717171] mt-2 pb-6'>{category.categoryDescription}</h6>
+              </div>
+              <div className='border-t border-neutral-200'>
+                {products.map((product: {
+                  id: string,
+                  restaurant: string,
+                  category: string,
+                  productName: string,
+                  productDescription: string,
+                  productValue: number,
+                  productFoto: string,
+                }) => (
+                  <>
+                    {product.category === category.id ? (
+                      <div className='pt-8 pb-8 border-b border-neutral-200 flex justify-between gap-4'>
+                        <img src={product.productFoto} alt="Product Foto" className='w-full max-w-[65px] max-h-[65px] h-full' />
+                        <div className='w-full'>
+                          <h1 className='font-bold'>{product.productName}</h1>
+                          <p className='text-sm'>{product.productDescription}</p>
+                        </div>
+                        <div className='flex gap-4 max-h-[40px]'>
+                          <div className='text-[#ea1d2c] border border-[#ea1d2c] w-[100px] flex items-center justify-center rounded-xl cursor-pointer'>Editar</div>
+                          <div className='bg-[#ea1d2c] text-white w-[100px] flex items-center justify-center rounded-xl cursor-pointer'>Remover</div>
+                        </div>
+                      </div>
+                    ) : (<></>)}
+                  </>
+                ))}
+                <div className='flex justify-between items-center w-full mt-12'>
+                  {editProductCategory ? (
+                    <Popup title={"Editar Categoria"} state={setEditProductCategory}>
+                      <form onSubmit={(e: React.SyntheticEvent) => {
+                        e.preventDefault()
+                        updateCategory(category.id)
+                      }} className='mt-14'>
+
+                        <label htmlFor="nome" className='text-lg'>Nome da Categoria</label>
+                        <input type="text" name="nome" id="nome" minLength={2} maxLength={35} className='w-full outline-none pl-4 pr-4 pt-2 pb-2 border border-neutral-200 rounded-lg mt-1 text-[#717171] mb-8' autoComplete='off' placeholder='Exemplo: Bebidas, Acompanhamentos, Hamburguers' onChange={(e) => setCategoryName(e.target.value)} defaultValue={category.categoryName} required />
+
+                        <label htmlFor="descricao" className='text-lg'>Descrição</label>
+                        <input type="text" name="descricao" id="descricao" minLength={2} maxLength={55} className='w-full outline-none pl-4 pr-4 pt-2 pb-2 border border-neutral-200 rounded-lg mt-1 text-[#717171] mb-8' autoComplete='off' placeholder='Descrição da categoria' onChange={(e) => setCategoryDescription(e.target.value)} defaultValue={category.categoryDescription} required />
+
+                        <button className='mt-12 w-full text-[#ea1d2c] rounded-xl p-4 text-center border border-[#ea1d2c] cursor-pointer' type='submit' onClick={() => removeCategory(category.id)}>
+                          Remover Categoria
+                        </button>
+                        <button className='mt-6 w-full bg-[#ea1d2c] rounded-xl p-4 text-center text-white font-bold cursor-pointer' type='submit'>
+                          Editar Categoria
+                        </button>
+                      </form>
+                    </Popup>
+                  ) : (<></>)}
+                  <div className='w-full'>
+                    <div className=' text-[#ea1d2c] border border-[#ea1d2c] max-w-[185px] flex items-center justify-center p-2 rounded-xl gap-2 cursor-pointer w-full' onClick={() => createProductAction(category.id)}>
+                      <IoAddOutline size={25} className="red-icon" />
+                      Adicionar Produto
+                    </div>
+                  </div>
+                  <div className='w-full flex justify-end text-[#ea1d2c] cursor-pointer underline-offset-4 underline' onClick={() => editCategory(category.id)}>
+                    Editar Categoria
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <div className='w-full flex flex-col items-center'>
+          <p className='max-w-[425px] text-center text-[#717171] mt-10'>Para adicionar um item, primeiramente é necessário adicionar uma seção de categoria de produto</p>
+        </div>
+      )}
+
+      <Button text='Adicionar Categoria' handleClick={() => setCreateProductCategory(true)} />
+
+
+      {createProduct ? (
+        <Popup title={"Adicionar Produto"} state={setCreateProduct}>
+          <form onSubmit={(e: React.SyntheticEvent) => {
+            e.preventDefault()
+            createNewProduct()
+          }} className='mt-14'>
+
+            <label htmlFor="nome" className='text-lg'>Nome do Produto</label>
+            <input type="text" name="nome" id="nome" minLength={2} maxLength={35} className='w-full outline-none pl-4 pr-4 pt-2 pb-2 border border-neutral-200 rounded-lg mt-1 text-[#717171] mb-8' autoComplete='off' placeholder='Exemplo: Feijoada, Pastel, etc.' onChange={(e) => setProductName(e.target.value)} required />
+
+            <label htmlFor="descricao" className='text-lg'>Descrição do Produto</label>
+            <input type="text" name="descricao" id="descricao" minLength={2} maxLength={55} className='w-full outline-none pl-4 pr-4 pt-2 pb-2 border border-neutral-200 rounded-lg mt-1 text-[#717171] mb-8' autoComplete='off' placeholder='Descrição do seu produto oferecido' onChange={(e) => setProductDescription(e.target.value)} required />
+
+            <label htmlFor="preco" className='text-lg'>Preço do Produto</label>
+            <input type="number" name="preco" id="preco" className='w-full outline-none pl-4 pr-4 pt-2 pb-2 border border-neutral-200 rounded-lg mt-1 text-[#717171] mb-8' autoComplete='off' placeholder='Qual o preço do seu produto?' onChange={(e) => setProductValue(e.target.value)} required />
+
+            <Upload setState={(value: any) => setProductFoto(value)} currentFoto={productFoto} />
+
+            {productName !== "" && productValue !== "" && productDescription !== "" && productFoto !== "" ? (
+              <button className='mt-12 w-full bg-[#ea1d2c] rounded-xl p-4 text-center text-white font-bold cursor-pointer' type='submit'>
+                Criar Produto
+              </button>
+            ) : (
+              <button className='mt-12 w-full bg-[#dddddd] rounded-xl p-4 text-center text-[#717171] font-bold cursor-not-allowed'>Criar Produto</button>
+            )}
+
+          </form>
+        </Popup>
+      ) : (<></>)}
+
+      {createProductCategory ? (
+        <Popup title={"Adicionar Categoria"} state={setCreateProductCategory}>
+          <form onSubmit={(e: React.SyntheticEvent) => {
+            e.preventDefault()
+            createCategory()
+          }} className='mt-14'>
+
+            <label htmlFor="nome" className='text-lg'>Nome da Categoria</label>
+            <input type="text" name="nome" id="nome" minLength={2} maxLength={35} className='w-full outline-none pl-4 pr-4 pt-2 pb-2 border border-neutral-200 rounded-lg mt-1 text-[#717171] mb-8' autoComplete='off' placeholder='Exemplo: Bebidas, Acompanhamentos, Hamburguers' onChange={(e) => setCategoryName(e.target.value)} required />
+
+            <label htmlFor="descricao" className='text-lg'>Descrição</label>
+            <input type="text" name="descricao" id="descricao" minLength={2} maxLength={55} className='w-full outline-none pl-4 pr-4 pt-2 pb-2 border border-neutral-200 rounded-lg mt-1 text-[#717171] mb-8' autoComplete='off' placeholder='Descrição da categoria' onChange={(e) => setCategoryDescription(e.target.value)} required />
+
+            {categoryName !== "" && categoryDescription !== "" ? (
+              <button className='mt-12 w-full bg-[#ea1d2c] rounded-xl p-4 text-center text-white font-bold cursor-pointer' type='submit'>
+                Criar Categoria
+              </button>
+            ) : (
+              <button className='mt-12 w-full bg-[#dddddd] rounded-xl p-4 text-center text-[#717171] font-bold cursor-not-allowed'>Criar Categoria</button>
+            )}
+          </form>
+        </Popup>
+      ) : (<></>)}
+    </>
+  )
+}
+
+export default Menu
